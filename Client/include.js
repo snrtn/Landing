800ce05468c5e33c040f74239acca6447567f77b
snrtn <snrtn94@gmail.com>
2021-12-20 00:35:24 +0100

    ajoute code

---
+++ Client/include.js
@@ -0,0 +1,18 @@
+// https://www.w3schools.com/howto/howto_html_include.asp
+
+window.addEventListener("load", function () {
+  var allElements = document.getElementsByTagName("*");
+  Array.prototype.forEach.call(allElements, function (el) {
+    var includePath = el.dataset.includePath;
+    if (includePath) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          el.outerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", includePath, true);
+      xhttp.send();
+    }
+  });
+});
